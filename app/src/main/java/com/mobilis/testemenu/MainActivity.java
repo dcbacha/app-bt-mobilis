583ab6c99b583389145867d3f0b469f6a25e6c09
Engenharia Mobilis <engenharia05@mobilis.me>
2017-06-28 15:33:15 -0300

    primeiro commit

---
+++ app/src/main/java/com/mobilis/testemenu/MainActivity.java
@@ -0,0 +1,119 @@
+package com.mobilis.testemenu;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+
+    private BluetoothAdapter mBluetoothAdapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        final Button btn_wifi = (Button) findViewById(R.id.button_wifi);
+        btn_wifi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                searchWIFI();
+            }
+        });
+
+        final Button btn_bt = (Button) findViewById(R.id.button_bt);
+        btn_bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                searchBT();
+
+            }
+        });
+
+
+    }
+
+    public void searchBT(){
+        Log.i(TAG, "searchBT()");
+        Intent serverIntent = new Intent(this, DeviceListActivity.class);
+        startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+    }
+
+    public void searchWIFI(){
+        Log.i(TAG, "searchWIFI()");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.insecure_connect_scan: {
+
+                Log.i(TAG, "clicou no insecure");
+                Intent serverIntent = new Intent(this, DeviceListActivity.class);
+                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
+                return true;
+            }
+            case R.id.discoverable: {
+
+                Log.i(TAG, "clicou no discoverable");
+                ensureDiscoverable();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+}
